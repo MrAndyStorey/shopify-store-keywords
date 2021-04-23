d0b8263611d90bbe6a88dbb01b70ee9fb8305e19
Andy Storey <betterseo@hotmail.co.uk>
2021-04-23 07:35:22 +0100

    Bug fixing

--- keywords.py
+++ keywords.py
@@ -1,21 +1,22 @@
+
+#!/usr/bin/env python3
+import requests
 import sys
 import getopt
-
 import time
-
 import string
-letters = string.ascii_lowercase
-
-from progress.bar import progressBar
+from progress.bar import Bar
 
 
-def main(argv):
+if __name__ == ‘__main__’:
 
   # By default it will go from aaa to zzz, but lets allow two parameters to be passed to the script to specifiy a start and end.
-  charStarts = "ccc"
-  charEnds = "ddd"
+  charStarts = "aaa"
+  charEnds = "zzz"
+
   try:
-    opts, args = getopt.getopt(argv,"s:e:",["starts=","ends="])
+    opts, args = getopt.getopt(argv,"s:e:",["ifile=","ofile="])
+
   except getopt.GetoptError:
     print 'keywords.py -starts <three-characters: aaa> -ends <three-characters: zzz>'
     sys.exit(2)
@@ -27,19 +28,14 @@ def main(argv):
 
   # First, build a list of strings for the script to attempt to retrieve KWs.  
   # Three character chars is the minimum that the xxx will accept.
+  letters = string.ascii_lowercase
   if charEnds > charStarts:
-    timeStart = time.perf_counter()
     builtArray=([''.join([a,b,c]) for a in letters for b in letters for c in letters if a+b+c >= charStarts and a+b+c <= charEnds])
-    timeEnd = time.perf_counter()
-    print(f"Concatenation: {timeStart - timeEnd:0.4f} seconds")
-
-  print(len(builtArray))
+    print(len(builtArray))
 
   # Due to a web request for each list element, this will take some time, so lets show the user a progress bar
-  with progressBar('Processing...') as bar:
+  with Bar('Processing...') as bar:
       for i in range(100):
           sleep(0.02)
           bar.next()
-          
-if __name__ == "__main__":
-   main(sys.argv[1:])
+
