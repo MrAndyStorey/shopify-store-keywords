1b88379483b145900d3ed394180193c8859c74b1
Andy Storey <betterseo@hotmail.co.uk>
2021-04-27 15:55:18 +0100

    Still some bugs to fix around ads in the results

--- process.py
+++ process.py
@@ -2,6 +2,7 @@
 import sys
 import string
 import re
+import csv
 
 import argparse
 
@@ -14,12 +15,14 @@ from progress.bar import Bar
 # Allow the user to pass the start and finish of the three-character list via a CLI argument.
 # By default it will go from aaa to zzz
 parser = argparse.ArgumentParser(description='Processes the input file using Beautiful Soup and then saves the data in the output file.')
-parser.add_argument("--input", default="test.txt", type=str, help="Input filename - default = keywords.txt.")
-parser.add_argument("--output", default="keyword_data.csv", type=str, help="Output filename - default = keyword_data.csv.")
+parser.add_argument("--input", default="/Users/andystorey/shopify-store-keywords/test.txt", type=str, help="Input filename - default = keywords.txt.")
+parser.add_argument("--output", default="/Users/andystorey/shopify-store-keywords/keyword_data.csv", type=str, help="Output filename - default = keyword_data.csv.")
 args = parser.parse_args()
 
 getURL = "https://apps.shopify.com/search?q="
-getURL = "https://webhook.site/66d175a7-bfd4-4943-8806-58c0e8510a80?q="
+#getURL = "https://webhook.site/66d175a7-bfd4-4943-8806-58c0e8510a80?q="
+
+topXLimit = 3
 
 if __name__ == '__main__':
     keywords = []
@@ -30,7 +33,8 @@ if __name__ == '__main__':
 
     # Open the output file and loop through each keyword one by one
     with open(args.output, 'w+', encoding='utf-8') as output_file:
-        output_file.write('Keyword,Results,Free,Paid,Avg Rating,Avg Reviews,Top 5\n')
+        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Keyword', 'Results', 'Free', 'Paid', 'Avg-Rating', 'Avg-Reviews', 'Top-' + str(topXLimit)])
 
         # Showing a progress bar to the user.
         with Bar('Processing ' + str(len(keywords)) + ' keywords.', max=len(keywords)) as bar:
@@ -38,46 +42,57 @@ if __name__ == '__main__':
             for keyword in keywords:
                 response = requests.get(getURL + keyword)
                 if response.status_code == 200:
-                    builtString = keyword + ","
+                    avgRating = []
+                    avgReviews = []
+
+                    builtResults = "0"
+                    builtFree = "0"
+                    builtPaid = "0"
+                    builtAvgRating = "0"
+                    builtAvgReviews = "0"
+                    builtTopX = ""
 
                     numResults = re.search("of\s(\d+)\sresults", response.text)
                     if numResults:
-                        builtString = builtString + "{},".format(str(numResults[1]))
+                        builtResults = "{}".format(str(numResults[1]))
                     else:
-                        builtString = builtString + "0,"
-
-                        
-                    avgRating = []
-                    avgRevivews = []
-                    avgCounter = 0
-
-
+                        builtResults = "0"
+                    
+                    #Create an object, passing the HTML text
                     soup = BeautifulSoup(response.text, 'html.parser')
 
                     #First, lets see the spilt between paid for and free apps - data is contained within id="PriceFilter", inside <li><a><span></span></a></li>
                     for pricingSection in soup.find_all(id="PriceFilter"):
                         priceItem = pricingSection.find_all("span", class_ = "search-filter-group__item-count")
                         if priceItem:
-                            builtString = builtString + "{},{},".format(str(priceItem[0].text).strip(),str(priceItem[1].text).strip())
+                            builtFree = "{}".format(str(priceItem[0].text).strip())
+                            builtPaid = "{}".format(str(priceItem[1].text).strip())
                         else:
-                            builtString = builtString + "-,-,"
+                            builtFree = "-"
+                            builtPaid = "-"
 
+                    #Now find out the average number of stars each app has (avgRating) and a count of all the reviews (avgReviews)
                     for searchResultItem in soup.find_all("div", class_ = "ui-app-card__review", limit=100):
-                        print(str(searchResultItem.find(class_ = "ui-star-rating__rating").text))
-                        print(str(searchResultItem.find(class_ = "ui-review-count-summary").text))
-                        
-                        #avgRating.append()
-                        #avgRevivews.append()
-                        avgCounter += 1
-                                           
-                    builtString = builtString + "{},{},".format(str(avgCounter),str(avgCounter))
+                        if searchResultItem.find(class_ = "ui-star-rating__rating"):
+                            searchRatings = re.search("(\d+)", searchResultItem.find(class_ = "ui-star-rating__rating").text)
+                            if searchRatings:
+                                avgRating.append(float(searchRatings[1]))
 
+                        if searchResultItem.find(class_ = "ui-review-count-summary"):
+                            searchReviews = re.search("([0-9].[0-9])", searchResultItem.find(class_ = "ui-review-count-summary").text)
+                            if searchReviews:
+                                avgReviews.append(int(searchReviews[1]))
 
-                    #Finally, find the names of the top 5 apps, we wil separate them with a pipe.
-                    for searchResultItem in soup.find_all("h2", class_ = "heading--4 ui-app-card__name", limit=5):
-                            builtString = builtString + "{}|".format(str(searchResultItem.text))
+                    builtAvgRating = "{}".format(round(sum(avgRating) / len(avgRating),2))
+                    builtAvgReviews = "{}".format(sum(avgReviews))
 
-                    output_file.write(builtString.rstrip('|') + '\n')
+                    #Finally, find the names of the top X apps, we wil separate them with a pipe.
+                    for searchResultItem in soup.find_all("h2", class_ = "heading--4 ui-app-card__name", limit=topXLimit):
+                            builtTopX = builtTopX + "{}|".format(str(searchResultItem.text))
+
+                    
+                    # Write the data for this row to the csv file.
+                    csv_writer.writerow([keyword, builtResults, builtFree, builtPaid, builtAvgRating, builtAvgReviews, builtTopX.rstrip('|')])
 
                 bar.next()
 
